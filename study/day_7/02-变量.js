82a47a6cb2b7c9caef1c66ba1856314500d7a07f
Ivelisya <138857469+Ivelisya@users.noreply.github.com>
2025-04-28 17:12:03 +0800

    feat: 新增语句和注释、变量及数据类型的示例页面，包含详细注释和代码示例

---
+++ study/day_7/02-变量.js
@@ -0,0 +1,23 @@
+var str = "hello world";
+console.log(str);
+// 2.先声明再赋值
+var str2;
+str2 = "hello world";
+console.log(str2);
+// var str3;
+// console.log(str3); // undefined
+var aa = 1, bb = 2, cc = 3;
+var str2;
+// console.log(bb, cc); // 1 2 3
+console.log(str2);
+
+var str = "你好";
+console.log(str); // 你好
+var flag = true;
+console.log(flag); // true
+// 3. 变量提升
+console.log(uname);//undefined
+var uname = "zhangsan"; 
+//没有变量提升
+upwd = "123456";
+console.log(upwd); // 123456
