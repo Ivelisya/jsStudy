19811d271ab974284f973116d951acd82ce34fce
Ivelisya <138857469+Ivelisya@users.noreply.github.com>
2025-06-10 23:14:44 +0800

    Update main.js

--- study/es6/main.js
+++ study/es6/main.js
@@ -1,25 +1,11 @@
-//变量和常量
-var a = 1;
-let b = 2;
-b++;
-{
-    const c = 3;
-    console.log(c);
-    // c = 4; // 会报错，因为c是常量
-}
-//const 常量 意思是不能被修改的量
-// 不能给常量分配一个可以变的值
+//1.promise async 同步和异步 常见的异步 定时器 ajax 事件循环
+console.log("任务1：..同步");
+console.log("任务2：..同步");
+setTimeout(() => {
+    console.log("任务4：..异步");
+}, 0);
+console.log("任务3：..同步");
 
-// console.log(c);
-//模板字符串
-const str1 = 'abc';
-//通过标记进行内容引用
-const str2 = 'efg${str1}这也是';
-console.log(str2);
+//2.proxy
 
-//3.解构赋值
-const arr = [1,2,3];
-const [x, y, z] = arr;
-console.log(x, y, z); // 1 2 3
-
-const result = arr 
+//3.module
